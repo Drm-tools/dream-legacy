7fcbab02b8ddf5a00aaf0e0e734cde1e3e63a1f3
corrados <corrados@users.sf.net>
2004-06-09 11:28:42 +0000

    New simulation type

--- common/SimulationParameters.cpp
+++ common/SimulationParameters.cpp
@@ -35,13 +35,10 @@ void CDRMSimulation::SimScript()
 	int				i;
 	_REAL			rStartSNR, rEndSNR, rStepSNR;
 	_REAL			rSNRCnt;
-	FILE*			pFileBitEr;
-	FILE*			pFileMSE;
+	FILE*			pFileSimRes;
 	CVector<_REAL>	vecrMSE;
 	string			strSimFile;
 	string			strSpecialRemark;
-	int				iSimTime = 0;
-	int				iSimNumErrors = 0;
 
 
 	/**************************************************************************\
@@ -51,9 +48,10 @@ void CDRMSimulation::SimScript()
 	   application will be started */
 	Param.eSimType = CParameter::ST_BITERROR;
 	Param.eSimType = CParameter::ST_MSECHANEST;
+	Param.eSimType = CParameter::ST_SYNC_PARAM; /* Check "SetSyncInput()"s! */
 	Param.eSimType = CParameter::ST_BER_IDEALCHAN;
-	Param.eSimType = CParameter::ST_NONE;
-	
+	Param.eSimType = CParameter::ST_NONE; /* No simulation, regular GUI */
+
 	if (Param.eSimType != CParameter::ST_NONE)
 	{
 		Param.iDRMChannelNum = 5;
@@ -76,12 +74,12 @@ void CDRMSimulation::SimScript()
 		else
 		{
 			Param.InitCellMapTable(RM_ROBUSTNESS_MODE_B, SO_3);
-			Param.eSymbolInterlMode = CParameter::SI_LONG;//SI_SHORT;//
+			Param.eSymbolInterlMode = CParameter::SI_LONG;
 		}
 
 		/* The associated code rate is R = 0,6 and the modulation is 64-QAM */
 		Param.MSCPrLe.iPartB = 1;
-		Param.eMSCCodingScheme = CParameter::CS_3_SM;//CS_3_HMMIX;//CS_3_HMSYM;//
+		Param.eMSCCodingScheme = CParameter::CS_3_SM;
 
 
 ChannelEstimation.SetFreqInt(CChannelEstimation::FWIENER);
@@ -135,49 +133,53 @@ ChannelEstimation.SetTimeInt(CChannelEstimation::TWIENER);
 			GenSimData.SetNumErrors(iSimNumErrors, 
 				SimFileName(Param, strSpecialRemark));
 
-		if (Param.eSimType == CParameter::ST_MSECHANEST)
+		/* Open file for storing simulation results */
+		strSimFile = string(SIM_OUT_FILES_PATH) +
+			SimFileName(Param, strSpecialRemark) + string(".dat");
+		pFileSimRes = fopen(strSimFile.c_str(), "w");
+		printf("%s\n", strSimFile.c_str()); /* Show name directly */
+
+		/* Main SNR loop */
+		for (rSNRCnt = rStartSNR; rSNRCnt <= rEndSNR; rSNRCnt += rStepSNR)
 		{
-			/* Open simulation file */
-			strSimFile = string(SIM_OUT_FILES_PATH) +
-				SimFileName(Param, strSpecialRemark) + string(".dat");
-			pFileMSE = fopen(strSimFile.c_str(), "w");
-
-			Param.rSimSNRdB = rStartSNR;
-
+			/* Set SNR in global struct and run simulation */
+			Param.rSimSNRdB = rSNRCnt;
 			Run();
 
-			/* After the simulation get results */
-			IdealChanEst.GetResults(vecrMSE);
-
-			/* Store results in a file */
-			for (i = 0; i < vecrMSE.Size(); i++)
-				fprintf(pFileMSE, "%e ", vecrMSE[i]);
-			fprintf(pFileMSE, "\n");
-			fclose(pFileMSE);
-		}
-		else
-		{
-			/* Open simulation file */
-			strSimFile = string(SIM_OUT_FILES_PATH) +
-				SimFileName(Param, strSpecialRemark) + string(".dat");
-			pFileBitEr = fopen(strSimFile.c_str(), "w");
-
-			for (rSNRCnt = rStartSNR; rSNRCnt <= rEndSNR; rSNRCnt += rStepSNR)
+			/* Store results in file */
+			switch (Param.eSimType)
 			{
-				Param.rSimSNRdB = rSNRCnt;
+			case CParameter::ST_MSECHANEST:
+				/* After the simulation get results */
+				IdealChanEst.GetResults(vecrMSE);
 
-				Run();
+				/* Store results in a file */
+				for (i = 0; i < vecrMSE.Size(); i++)
+					fprintf(pFileSimRes, "%e ", vecrMSE[i]);
+				fprintf(pFileSimRes, "\n"); /* New line */
+				break;
 
-				/* Show results directly */
+			case CParameter::ST_SYNC_PARAM:
+				/* Show results directly and save them in the file */
+				printf("%e %e\n", rSNRCnt, Param.rSyncTestParam);
+				fprintf(pFileSimRes, "%e %e\n", rSNRCnt, Param.rSyncTestParam);
+				break;
+
+			default:
+				/* Show results directly and save them in the file */
 				printf("%e %e\n", rSNRCnt, Param.rBitErrRate);
-
-				/* Save results */
-				fprintf(pFileBitEr, "%e %e\n", rSNRCnt, Param.rBitErrRate);
-				fflush(pFileBitEr);
+				fprintf(pFileSimRes, "%e %e\n", rSNRCnt, Param.rBitErrRate);
+				break;
 			}
 
-			fclose(pFileBitEr);
+			/* Make sure results are actually written in the file. In case the
+			   simulation machine crashes, at least the last results are
+			   preserved */
+			fflush(pFileSimRes);
 		}
+
+		/* Close simulation results file afterwards */
+		fclose(pFileSimRes);
 	}
 
 
@@ -188,16 +190,21 @@ ChannelEstimation.SetTimeInt(CChannelEstimation::TWIENER);
 
 string CDRMSimulation::SimFileName(CParameter& Param, string strAddInf)
 {
-	/* File naming convention:
-	   BER: Bit error rate simulation
-	   MSE: MSE for channel estimation
+/* 
+	File naming convention:
+	BER: Bit error rate simulation
+	MSE: MSE for channel estimation
+	BERIDEAL: Bit error rate simulation with ideal channel estimation
+	SYNC: Simulation for a synchronization paramter (usually variance)
 
-	   B3: Robustness mode and spectrum occupancy
-	   Ch5: Which channel was used
+	B3: Robustness mode and spectrum occupancy
+	Ch5: Which channel was used
 
-	   10k: Value set by "SetNumErrors()"
+	10k: Value set by "SetNumErrors()"
 
-	   example: BER_B3_Ch5_10k_NoSync */
+	example: BER_B3_Ch5_10k_NoSync
+*/
+	char chNumTmpLong[10];
 	string strFileName = "";
 
 	/* What type of simulation */
@@ -211,6 +218,9 @@ string CDRMSimulation::SimFileName(CParameter& Param, string strAddInf)
 		break;
 	case CParameter::ST_BER_IDEALCHAN:
 		strFileName += "BERIDEAL_";
+		break;
+	case CParameter::ST_SYNC_PARAM:
+		strFileName += "SYNC_";
 		break;
 	}
 
@@ -259,11 +269,68 @@ string CDRMSimulation::SimFileName(CParameter& Param, string strAddInf)
 		break;
 	}
 
-	/* Number of error events */
-	char chNumTmpLong[10];
-	sprintf(chNumTmpLong, "%d", GenSimData.GetNumErrors() / 1000);
+	/* Number of iterations in MLC decoder */
+	strFileName += "It";
+	sprintf(chNumTmpLong, "%d", MSCMLCDecoder.GetInitNumIterations());
 	strFileName += chNumTmpLong;
-	strFileName += "k";
+	strFileName += "_";
+
+	/* Protection level part B */
+	strFileName += "PL";
+	sprintf(chNumTmpLong, "%d", Param.MSCPrLe.iPartB);
+	strFileName += chNumTmpLong;
+	strFileName += "_";
+
+	/* MSC coding scheme */
+	switch (Param.eMSCCodingScheme)
+	{
+	case CParameter::CS_2_SM:
+		strFileName += "16SM_";
+		break;
+
+	case CParameter::CS_3_SM:
+		strFileName += "64SM_";
+		break;
+
+	case CParameter::CS_3_HMMIX:
+		strFileName += "64MIX_";
+		break;
+
+	case CParameter::CS_3_HMSYM:
+		strFileName += "64SYM_";
+		break;
+	}
+
+	/* Number of error events or simulation time */
+	int iCurNum;
+	string strMultPl = "";
+	if (iSimTime != 0)
+	{
+		strFileName += "T"; /* T -> time */
+		iCurNum = iSimTime;
+	}
+	else
+	{
+		strFileName += "E"; /* E -> errors */
+		iCurNum = iSimNumErrors;
+	}
+	
+	if (iCurNum / 1000 > 0)
+	{
+		strMultPl = "K";
+		iCurNum /= 1000;
+
+		if (iCurNum / 1000 > 0)
+		{
+			strMultPl = "M";
+			iCurNum /= 1000;
+		}
+	}
+
+	sprintf(chNumTmpLong, "%d", iCurNum);
+	strFileName += chNumTmpLong;
+	strFileName += strMultPl;
+
 
 	/* Special remark */
 	if (!strAddInf.empty())
