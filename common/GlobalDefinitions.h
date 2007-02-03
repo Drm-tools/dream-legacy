d30c570e439b0a1fb24cb8e1d281a341bf0063e8
jcable <julian.cable@yahoo.com>
2007-02-03 19:19:53 +0000

    commented out typedefs that collide in some environments. TODO - find out why.

--- common/GlobalDefinitions.h
+++ common/GlobalDefinitions.h
@@ -149,13 +149,14 @@ typedef unsigned char/*bool*/			_BINARY;
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
 typedef unsigned __int16 uint16_t;
-typedef signed __int16 int16_t;
-typedef signed __int32 int32_t;
+// TODO find the other definitions of these and reconcile
+//typedef signed __int16 int16_t;
+//typedef signed __int32 int32_t;
 typedef unsigned __int32 uint32_t;
 typedef signed __int64 int64_t;
 typedef unsigned __int64 uint64_t;
 /* keep faac.h happy */
-#define HAVE_INT32_T
+//#define HAVE_INT32_T
 #else
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
