231549623959afb704b8c3eedda5a4000c9b08d3
Andrea Russo <andrusso@yahoo.com>
2007-04-04 09:14:45 +0000

    fix problems with winsock include

--- common/GPSReceiver.h
+++ common/GPSReceiver.h
@@ -32,7 +32,10 @@
 #include "GlobalDefinitions.h"
 #include <qsocket.h>
 #include <qthread.h>
-#include <qmutex.h>
+
+# if QT_VERSION >= 0x030000
+#  include <qmutex.h>
+# endif
 
 class CAutoMutex
 {
